3b362222834253571f848623576fdbd643301a4d
Vinícius Augusto <hat.ubuntu@gmail.com>
2024-09-24 21:04:32 -0300

    feat: adição dos atributos, criação do construtor e dos getters e setters

--- src/entities/locadora/Aluguel.java
+++ src/entities/locadora/Aluguel.java
@@ -1,4 +1,65 @@
 package entities.locadora;
 
+import entities.usuario.Cliente;
+import entities.veiculo.Veiculo;
+
+import java.time.LocalDate;
+
 public class Aluguel {
+
+    private Cliente cliente;
+    private Veiculo veiculo;
+    private LocalDate dataAluguel;
+    private LocalDate dataDevolucao;
+    private String localAluguel;
+    private String localDevolucao;
+    private boolean isDevolvido;
+
+
+    public Aluguel(Cliente cliente, Veiculo veiculo, LocalDate dataAluguel, String localAluguel){
+        this.cliente = cliente;
+        this.veiculo = veiculo;
+        this.dataAluguel = dataAluguel;
+        this.localAluguel = localAluguel;
+        this.isDevolvido = false;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+
+    public Veiculo getVeiculo() {
+        return veiculo;
+    }
+
+
+    public LocalDate getDataAluguel() {
+        return dataAluguel;
+    }
+
+
+    public LocalDate getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+
+
+    public String getLocalAluguel() {
+        return localAluguel;
+    }
+
+
+
+    public String getLocalDevolucao() {
+        return localDevolucao;
+    }
+
+
+
+    public boolean isDevolvido() {
+        return isDevolvido;
+    }
+
+
 }
