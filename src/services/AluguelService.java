509cab05be18284749b10cbc1cccd9c38f03d457
Vinícius Augusto <hat.ubuntu@gmail.com>
2024-09-25 10:38:52 -0300

    feat: implementado método para alugar e alterar a disponibilidade

--- src/services/AluguelService.java
+++ src/services/AluguelService.java
@@ -1,4 +1,31 @@
 package services;
 
-public class AluguelRepository {
+import entities.locadora.Aluguel;
+import entities.usuario.Cliente;
+import entities.veiculo.Veiculo;
+import repositories.AluguelRepository;
+
+import java.time.LocalDate;
+
+public class AluguelService {
+
+    private final AluguelRepository aluguelRepository;
+
+    public AluguelService(AluguelRepository aluguelRepository) {
+        this.aluguelRepository = aluguelRepository;
+    }
+
+
+    public void alugarVeiculo(Cliente cliente, Veiculo veiculo, LocalDate dataAluguel, String localAluguel){
+
+        Aluguel novoAluguel = new Aluguel(cliente, veiculo, dataAluguel, localAluguel);
+
+        novoAluguel.alugarVeiculo();
+
+        aluguelRepository.salvarAluguel(novoAluguel);
+
+        System.out.println(novoAluguel.gerarComprovanteDeAluguel());
+    }
+
+
 }
