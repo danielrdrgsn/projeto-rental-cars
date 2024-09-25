946080a46932d0b13f6861dbef353de3d42e5aa8
Vinícius Augusto <hat.ubuntu@gmail.com>
2024-09-24 21:23:16 -0300

    feat: add interface para gerenciamento dos alugueis

--- src/repositories/AluguelRepository.java
+++ src/repositories/AluguelRepository.java
@@ -1,4 +1,18 @@
 package repositories;
 
-public class AluguelRepository {
+import entities.locadora.Aluguel;
+import entities.usuario.Cliente;
+import entities.veiculo.Veiculo;
+
+import java.util.List;
+
+public interface AluguelRepository {
+
+    void salvarAluguel(Aluguel aluguel);
+
+    List<Aluguel> buscarTodos();
+    List<Aluguel> buscarPorCliente(Cliente cliente);
+    List<Aluguel> buscarPorVeiculo(Veiculo veiculo);
+
+
 }
