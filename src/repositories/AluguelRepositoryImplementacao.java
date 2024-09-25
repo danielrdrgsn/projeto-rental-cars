5a61d547c8454fa24b20f196099d947996fd8a93
Vinícius Augusto <hat.ubuntu@gmail.com>
2024-09-25 09:43:39 -0300

    feat: adicionando o construtor e implementando os métodos

--- src/repositories/AluguelRepositoryImplementacao.java
+++ src/repositories/AluguelRepositoryImplementacao.java
@@ -1,4 +1,37 @@
 package repositories;
 
-public class AluguelRepositoryImplementacao {
+import entities.locadora.Aluguel;
+import entities.usuario.Cliente;
+import entities.veiculo.Veiculo;
+
+import java.util.List;
+
+public class AluguelRepositoryImplementacao implements AluguelRepository{
+
+
+    private List<Aluguel> aluguels;
+
+    public AluguelRepositoryImplementacao(List<Aluguel> aluguels) {
+        this.aluguels = aluguels;
+    }
+
+    @Override
+    public void salvarAluguel(Aluguel aluguel) {
+
+    }
+
+    @Override
+    public List<Aluguel> buscarTodos() {
+        return List.of();
+    }
+
+    @Override
+    public List<Aluguel> buscarPorCliente(Cliente cliente) {
+        return List.of();
+    }
+
+    @Override
+    public List<Aluguel> buscarPorVeiculo(Veiculo veiculo) {
+        return List.of();
+    }
 }
